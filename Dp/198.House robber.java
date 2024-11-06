e0a0207b0bffa79b3f01ac8ee6efa523071a0999
Kartik <142515173+karts43@users.noreply.github.com>
2024-11-06 23:19:15 +0530

    198.House robber.java

--- Dp/198.House robber.java
+++ Dp/198.House robber.java
@@ -1,1 +1,26 @@
-
+// T.c = O(n) 
+// S.c = O(1)
+class Solution {
+  // step 111
+    public int rob(int[] nums) {
+      // step 1
+        int [] dp = new int[101];
+      // step 2
+        Arrays.fill(dp,-1);
+      // step final call(111.3)
+        return solve(nums,0,dp);
+    }
+  // step 111.3
+    public int solve(int []nums,int i,int []dp){
+      // step 3.0
+        if(i>=nums.length)return 0;
+      // step 3.1
+        if(dp[i] != -1)return dp[i];
+      // step 3->111.3
+        int stile = nums[i] + solve(nums,i+2,dp);
+       // step 4->111.3
+        int not_stile = solve(nums,i+1,dp);
+       // step 5-> (3 or 4) ->111.1
+        return dp[i]=Math.max(stile,not_stile);
+    }
+}
